4c2bdd7b9ac7fc9ea65cf738f3bc5d7d1d7baea4
cloudcome <cloudcome@163.com>
2014-09-20 11:48:31 +0800

    140920

---
+++ src/core/event/Emitter.js
@@ -0,0 +1,15 @@
+/*!
+ * Emitter.js
+ * @author ydr.me
+ * 2014-09-20 11:48
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/Emitter
+     */
+    'use strict';
+
+    module.exports = {};
+});
