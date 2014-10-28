9acdd6e5981318b5983db9c74c126767613edd6e
cloudcome <cloudcome@163.com>
2014-10-28 19:07:26 +0800

    ui/Imgclip

--- src/ui/Imgclip/index.js
+++ src/ui/Imgclip/index.js
@@ -7,7 +7,16 @@
 
 define(function (require, exports, module) {
     /**
-     * @module parent/index
+     * @todo 增加 ui/Resize，独立出来
+     * @module ui/Imgclip/index
+     * @module libs/Template
+     * @module libs/Emitter
+     * @module util/data
+     * @module util/class
+     * @module core/dom/selector
+     * @module core/dom/modification
+     * @module core/dom/attribute
+     * @module core/dom/drag
      */
     'use strict';
 
@@ -20,7 +29,7 @@ define(function (require, exports, module) {
     var selector = require('../../core/dom/selector.js');
     var modification = require('../../core/dom/modification.js');
     var attribute = require('../../core/dom/attribute.js');
-    var event = require('../../core/event/base.js');
+    var event = require('../../core/event/drag.js');
     var alienIndex = 1;
     var defaults = {
         minWidth: 100,
@@ -69,15 +78,165 @@ define(function (require, exports, module) {
             modification.insert($wrap, $ele, 'afterend');
             attribute.css($wrap, {
                 position: 'absolute',
-                width: attribute.width($ele),
-                height: attribute.height($ele)
+                width: the._width = attribute.width($ele),
+                height: the._height = attribute.height($ele)
             });
             attribute.top($wrap, attribute.top($ele));
             attribute.left($wrap, attribute.left($ele));
-            the._$election = selector.query('.alien-ui-imgclip-selection', $wrap)[0];
+            the._$sele = selector.query('.alien-ui-imgclip-selection', $wrap)[0];
             the._$bg = selector.query('.alien-ui-imgclip-bg', $wrap)[0];
-            modification.insert($img, the._$election, 'beforeend');
+            the._$in = selector.query('.alien-ui-imgclip-in', $wrap)[0];
+            modification.insert($img, the._$sele, 'afterbegin');
             the._$img = $img;
+            the.x0 = 0;
+            the.y0 = 0;
+            the.x1 = 0;
+            the.y1 = 0;
+            the._on();
+        },
+        _on: function () {
+            var the = this;
+            var left0;
+            var top0;
+            var width0;
+            var height0;
+            var x0;
+            var y0;
+            // 0 未拖动
+            // 1 正在拖动
+            // 2 已有选区
+            // 3 改变选区
+            var state = 0;
+
+            event.on(the._$wrap, 'dragstart', function (eve) {
+                eve.preventDefault();
+                var left;
+                var top;
+
+                if (state === 0) {
+                    left0 = attribute.left(the._$wrap);
+                    top0 = attribute.top(the._$wrap);
+                    x0 = eve.pageX;
+                    y0 = eve.pageY;
+                    state = 1;
+                    attribute.css(the._$bg, 'display', 'block');
+                    attribute.css(the._$sele, {
+                        display: 'block',
+                        left: left = x0 - left0,
+                        top: top = y0 - top0,
+                        width: 0,
+                        height: 0
+                    });
+                    attribute.css(the._$img, {
+                        left: -left,
+                        top: -top
+                    });
+                }
+            });
+
+            event.on(the._$wrap, 'drag', function (eve) {
+                var deltaX;
+                var deltaY;
+
+                eve.preventDefault();
+
+                if (state === 1) {
+                    deltaX = eve.pageX - x0;
+                    deltaY = eve.pageY - y0;
+                    attribute.css(the._$sele, {
+                        width: deltaX,
+                        height: deltaY
+                    });
+                } else if (state === 2) {
+
+                }
+            });
+
+            event.on(the._$wrap, 'dragend', function (eve) {
+                eve.preventDefault();
+
+                if (state === 1) {
+                    state = 2;
+                }
+            });
+
+            event.on(the._$in, 'dragstart', function (eve) {
+                eve.preventDefault();
+
+                if (state === 2) {
+                    state = 3;
+                    left0 = data.parseFloat(attribute.css(the._$sele, 'left'), 0);
+                    top0 = data.parseFloat(attribute.css(the._$sele, 'top'), 0);
+                    width0 = data.parseFloat(attribute.css(the._$sele, 'width'), 0);
+                    height0 = data.parseFloat(attribute.css(the._$sele, 'height'), 0);
+                    x0 = eve.pageX;
+                    y0 = eve.pageY;
+                    console.log('x0 => ' + x0);
+                }
+            });
+            event.on(the._$in, 'drag', function (eve) {
+                eve.preventDefault();
+
+                var deltaX;
+                var deltaY;
+                var left;
+                var top;
+                var maxLeft;
+                var maxTop;
+
+                if (state === 3) {
+                    deltaX = eve.pageX - x0;
+                    deltaY = eve.pageY - y0;
+                    left = left0 + deltaX;
+                    top = top0 + deltaY;
+                    maxLeft = the._width - width0;
+                    maxTop = the._height - height0;
+
+                    if (left <= 0) {
+                        left = 0;
+                    } else if (left >= maxLeft) {
+                        left = maxLeft;
+                    }
+
+                    if (top <= 0) {
+                        top = 0;
+                    } else if (top >= maxTop) {
+                        top = maxTop;
+                    }
+
+                    attribute.css(the._$sele, {
+                        left: left,
+                        top: top
+                    });
+                    attribute.css(the._$img, {
+                        left: -left,
+                        top: -top
+                    });
+                }
+            });
+            event.on(the._$in, 'dragend', function (eve) {
+                eve.preventDefault();
+
+                if (state === 3) {
+                    state = 2;
+                }
+            });
+
+            event.on(the._$bg, 'click', function () {
+                if (state === 2) {
+                    state = 0;
+                    attribute.css(the._$bg, 'display', 'none');
+                    attribute.css(the._$sele, 'display', 'none');
+                }
+            });
+        },
+        _un: function () {
+
+        },
+        destroy: function () {
+            var the = this;
+
+            the._un();
         }
     }, Emitter);
 
