5cfd003f83c10d425f62d583a9759cff13b6d24e
cloudcome <cloudcome@163.com>
2014-10-31 09:58:29 +0800

    imgClip

--- examples/ui/Imgclip.js
+++ examples/ui/Imgclip.js
@@ -1,7 +1,7 @@
 define(function (require) {
     var Imgclip = require('/src/ui/Imgclip/index.js');
     var $ret = document.getElementById('ret');
-    var imgclip = new Imgclip('#img',{
+    var imgclip = new Imgclip('#img', {
         minWidth: 100,
         minHeight: 100,
         ratio: 1.5
