dee9aa35bc08b985f7aaf4a2228ab4d0ff34d374
cloudcome <cloudcome@163.com>
2014-11-19 01:55:22 +0800

    ui/Banner

--- examples/ui/Banner.js
+++ examples/ui/Banner.js
@@ -7,39 +7,98 @@
 define(function (require) {
     'use strict';
 
+//var generator = require('/src/ui/generator.js');
+//var MyUI = generator({
+//    /**
+//     * 构造函数
+//     */
+//    constructor: function () {
+//        var the = this;
+//
+//        // 自动初始化
+//        the._init();
+//    },
+//
+//
+//    /**
+//     * 初始化
+//     * @private
+//     */
+//    _init: function () {
+//        var the = this;
+//
+//        the._initData();
+//        the._initNode();
+//        the._initEvent();
+//    },
+//
+//
+//    /**
+//     * 初始化数据
+//     * @private
+//     */
+//    _initData: function () {
+//        // code
+//    },
+//
+//
+//    /**
+//     * 初始化节点
+//     * @private
+//     */
+//    _initNode: function () {
+//        // code
+//    },
+//
+//
+//    /**
+//     * 初始化事件
+//     * @private
+//     */
+//    _initEvent: function () {
+//        // code
+//    },
+//
+//
+//    /**
+//     * 销毁实例
+//     */
+//    destroy: function () {
+//        // 1、节点的还原归位
+//        // 2、事件的解除绑定
+//    }
+//});
+
     var Banner = require('/src/ui/Banner/index.js');
     var $index = document.getElementById('index');
-    var bn = new Banner('#slider1', {
-        width: 700,
-        height: 300
+    var banner = new Banner('#slider1', {
+        width: 460,
+        height: 200,
+        axis: '+y'
     });
 
-    bn.on('change', function (index) {
+    banner.on('change', function (index) {
         $index.innerHTML = index + 1;
     });
 
     document.getElementById('prev').onclick = function () {
-        bn.prev();
+        banner.prev();
     };
 
     document.getElementById('next').onclick = function () {
-        bn.next();
+        banner.next();
     };
 
-    document.getElementById('play1').onclick = function () {
-        bn.play(1);
-    };
-
-    document.getElementById('play2').onclick = function () {
-        bn.play(-1);
+    document.getElementById('play').onclick = function () {
+        banner.play();
     };
 
     document.getElementById('pause').onclick = function () {
-        bn.pause();
+        banner.pause();
     };
 
     document.getElementById('destroy').onclick = function () {
-        bn.destroy();
-        bn = null;
+        banner.destroy();
+        banner = null;
     };
 });
