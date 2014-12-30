5770415baa976744fb2b57301a54e3ae3a28dd3f
cloudcome <cloudcome@163.com>
2014-12-30 22:06:52 +0800

    example

--- examples/libs/Template.js
+++ examples/libs/Template.js
@@ -5,6 +5,7 @@ define(function (require) {
     var $textarea = document.getElementById('template');
     var $render = document.getElementById('render');
     var $btn = document.getElementById('btn');
+    var $data = document.getElementById('data');
     var dd = {
         name: 'Jane Yim',
         love: '<b>FED</b>',
@@ -14,14 +15,20 @@ define(function (require) {
         },{
             type: '文学',
             list: ['百年孤独', '飘']
-        }]
+        }],
+        user: {
+            email: null
+        },
+        bio: undefined
     };
     var t = new Template($textarea.value);
 
     $render.innerHTML = t.render(dd);
+    $data.innerHTML = JSON.stringify(dd, null, 4);
 
     $btn.onclick = function () {
         var t = new Template($textarea.value);
         $render.innerHTML = t.render(dd);
+        $data.innerHTML = JSON.stringify(dd, null, 4);
     };
 });
