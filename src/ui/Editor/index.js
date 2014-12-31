425825c8eeb292dc5a10ae200f8ac7cb634f2731
cloudcome <cloudcome@163.com>
2014-12-31 12:11:21 +0800

    ui/Editor

--- src/ui/Editor/index.js
+++ src/ui/Editor/index.js
@@ -108,7 +108,6 @@ define(function (require, exports, module) {
             the._calStoreId();
             attribute.css(the._$ele, {
                 display: 'block',
-                border: 0,
                 width: '100%',
                 margin: 0
             });
