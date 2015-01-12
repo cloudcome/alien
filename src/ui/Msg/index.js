4bfa0da7b490726ff1d4438e1113ab4e68e46327
云淡然 <cloudcome@163.com>
2015-01-12 15:07:24 +0800

    ui/Msg/

--- src/ui/Msg/index.js
+++ src/ui/Msg/index.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * Msg
  * @author ydr.me
  * @create 2015-01-12 10:47
  */
@@ -7,11 +7,52 @@
 
 define(function (require, exports, module) {
     /**
-     * @module parent/index
+     * @module ui/Msg/
+     * @module ui/Mask/
+     * @module ui/Window/
      */
+
     'use strict';
 
-    var abc = require('./abc.js');
+    var Mask = require('../Mask/');
+    var Window = require('../Window/');
+    var ui = require('../base.js');
+    var dato = require('../../util/dato.js');
+    var typeis = require('../../util/typeis.js');
+    var alienIndex = 0;
+    var alienClass = 'alien-ui-msg';
+    var defaults = {
+        width: 300,
+        height: 'auto',
+        left: 'center',
+        top: 'center',
+        title: '提示',
+        content: 'Hello world!',
+        addClass: '',
+        buttons: null,
+        canDrag: true,
+        isModal: true,
+        timeout: -1
+    };
+    var Msg = ui.create({
+        constructor: function (options) {
+            var the = this;
 
-    module.exports = {};
+            if (typeis.string(options)) {
+                options = {
+                    content: options
+                };
+            }
+
+            the._options = dato.extend(true, {}, defaults, options);
+            the._init();
+        },
+
+
+        _init: function () {
+
+        }
+    });
+
+    module.exports = Msg;
 });
