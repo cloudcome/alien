565f4a916499aa1b3be1f23c7f8f673c50ea4f3c
cloudcome <cloudcome@163.com>
2015-04-03 04:51:13 +0800

    rich markdown editor

--- src/ui/Editor/index.js
+++ src/ui/Editor/index.js
@@ -12,7 +12,9 @@ define(function (require, exports, module) {
     var ui = require('../');
     var selector = require('../../core/dom/selector.js');
     var Editor = ui.create(function ($editor) {
-        this._editor = gfm.fromTextArea(selector.query($editor)[0], {
+        var the = this;
+
+        the._editor = gfm.fromTextArea(selector.query($editor)[0], {
             mode: 'gfm',
             lineNumbers: true,
             theme: "monokai",
@@ -30,7 +32,30 @@ define(function (require, exports, module) {
             styleSelectedText: true,
             tabSize: 4
         });
+        the._initEvent();
     });
+
+
+    Editor.fn._initEvent = function () {
+        var the = this;
+
+        the._editor.addKeyMap({
+            'Cmd-B': function () {
+                var raw = the._editor.getSelection();
+
+                if(raw){
+                    the._editor.replaceSelection('**' + raw + '**');
+                }
+            },
+            'Cmd-I': function () {
+                var raw = the._editor.getSelection();
+
+                if(raw){
+                    the._editor.replaceSelection('*' + raw + '*');
+                }
+            }
+        });
+    };
 
     Editor.fn.getValue = function () {
         return this._editor.getValue();
