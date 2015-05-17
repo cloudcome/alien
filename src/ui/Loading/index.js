633bbae51cff5c064268ae47fdbaefd46cbbf545
张云来 <cloudcome@163.com>
2015-05-17 23:44:39 +0800

    matrix

--- src/ui/Loading/index.js
+++ src/ui/Loading/index.js
@@ -1,8 +1,58 @@
 /*!
  * loading ui
+ * @todo 目前使用 svg 来实现可定制的 loading，在 mac chrome 下，CPU 增高？？
  * @author ydr.me
  * @create 2015-05-15 10:19
  */
+
+
+/*************
+ <div class="loader">Loading...</div>
+
+ .loader {
+  margin: 6em auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #ffffff;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+ .loader,
+ .loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+ @-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+ @keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+*************/
+
 
 
 define(function (require, exports, module) {
