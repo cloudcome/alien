f916a4b38e4cdee454a2075d4883dc95f529928d
张云来 <cloudcome@163.com>
2015-06-30 11:24:54 +0800

    lowercase

--- examples/test/test.js
+++ examples/test/test.js
@@ -2,4 +2,24 @@ define(function (require) {
     var Dangke = require('../../src/libs/Dangke.js');
 
     document.body.innerHTML = Dangke.isDangke;
+
+
+    function dfn(obj){
+        return Object.defineProperties(obj, {
+            name: {
+                get: function () {
+                    console.log('get');
+                    return this.name2;
+                },
+                set: function (val) {
+                    console.log('set', val);
+                    this.name2 = val;
+                }
+            }
+        });
+    }
+
+    var o = dfn({});
+
+    o.name = 'xx';
 });
