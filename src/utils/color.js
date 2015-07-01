c76d3566bf1511d24b78c5aea0dbf6ac9712c9c9
张云来 <cloudcome@163.com>
2015-07-01 09:54:09 +0800

    color and so on

---
+++ src/utils/color.js
@@ -0,0 +1,19 @@
+/*!
+ * 颜色
+ * @author ydr.me
+ * @create 2015-07-01 00:13
+ * @ref https://github.com/mbjordan/Colors/blob/master/colors.js
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/color
+     */
+
+    'use strict';
+
+    var abc = require('./abc.js');
+
+    module.exports = {};
+});
