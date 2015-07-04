bcb9ef5d374abb8e696f95eebea3f843e0f13930
张云来 <cloudcome@163.com>
2015-07-05 00:33:08 +0800

    linkage

--- examples/ui/linkage.js
+++ examples/ui/linkage.js
@@ -13,12 +13,15 @@ define(function (require, exports, module) {
     'use strict';
 
     var Linkage = require('../../src/ui/linkage/index.js');
-
-    new Linkage('#demo', {
+    var linkage = new Linkage('#demo', {
         urls: [
             './linkage1.json',
             './linkage2.json',
             './linkage3.json'
         ]
+    });
+
+    linkage.on('change', function () {
+        console.log(this.values);
     });
 });
--- src/ui/linkage/index.js
+++ src/ui/linkage/index.js
@@ -17,7 +17,9 @@ define(function (require, exports, module) {
     var selector = require('../../core/dom/selector.js');
     var modification = require('../../core/dom/modification.js');
     var attribute = require('../../core/dom/attribute.js');
+    var event = require('../../core/event/base.js');
     var xhr = require('../../core/communication/xhr.js');
+    var namespace = 'alien-ui-linkage';
     var defaults = {
         // 数据的 text 键名
         textName: 'text',
@@ -69,6 +71,10 @@ define(function (require, exports, module) {
             } else {
                 the._$selects = selector.query('select', the._$parent);
             }
+
+            dato.repeat(the._length, function (index) {
+                the._$selects[index][namespace + 'index'] = index;
+            });
         },
 
 
@@ -82,6 +88,21 @@ define(function (require, exports, module) {
 
             // 获取到了列表数据
             the.on('list', the._renderList);
+
+            dato.repeat(the._length, function (index) {
+                event.on(the._$selects[index], 'change', the._onchange = function () {
+                    var index = this[namespace + 'index'];
+                    var value = this.value;
+                    var nextIndex = index + 1;
+
+                    the.values[index] = value;
+                    the.emit('change', index, value);
+
+                    if (nextIndex < the._length) {
+                        the.change(index + 1, value);
+                    }
+                });
+            });
         },
 
 
