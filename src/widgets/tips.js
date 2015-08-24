c8fa16a0b5833a3d17bdf790705b3a023823483d
张云来 <cloudcome@163.com>
2015-08-24 14:19:43 +0800

    tip -> tips

--- src/widgets/tips.js
+++ src/widgets/tips.js
@@ -24,24 +24,26 @@ define(function (require, exports, module) {
     var controller = require('../utils/controller.js');
     var event = require('../core/event/base.js');
     var attribute = require('../core/dom/attribute.js');
+    var namespace = 'alien-widgets-tips';
     var defaults = {
         width: 'auto',
         minWidth: 100,
         title: null,
         buttons: [],
-        addClass: 'alien-widgets-tip',
+        addClass: '',
         sureIndex: 0,
         isModal: false,
         timeout: 3456,
         autoFocus: false
     };
     var lastMsg = null;
-    //var maybeMobile = 'ontouchend' in document;
 
     module.exports = function (content, options) {
         options = dato.extend({}, defaults, options, {
             content: content && content.message ? content.message : String(content)
         });
+
+        options.addClass += ' ' + namespace;
 
         if (lastMsg) {
             lastMsg.destroy();
@@ -54,19 +56,5 @@ define(function (require, exports, module) {
         return lastMsg;
     };
 
-    //if (maybeMobile) {
-    //    event.on(document, 'focusin focusout', 'input,textarea,select', controller.debounce(function () {
-    //        if (lastMsg) {
-    //            var onposition;
-    //
-    //            lastMsg.before('position', onposition = function (to) {
-    //                to.marginTop = attribute.scrollTop(window) / 2;
-    //                lastMsg.un('beforeposition', onposition);
-    //            });
-    //            lastMsg.position();
-    //        }
-    //    }, 345, false));
-    //}
-
-    ui.importStyle(require('./tip.css', 'css'));
+    ui.importStyle(require('./tips.css', 'css'));
 });
