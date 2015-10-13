6915449cd0e0332d571b6ebd496ecce614e97cde
张云来 <cloudcome@163.com>
2015-10-13 11:30:58 +0800

    spa

--- examples/ui/spa.js
+++ examples/ui/spa.js
@@ -9,13 +9,19 @@ define(function (require, exports, module) {
     'use strict';
 
     var SPA = require('../../src/ui/spa/index.js');
+    var selector = require('../../src/core/dom/selector.js');
+    var attribute = require('../../src/core/dom/attribute.js');
     var spa = new SPA('#view');
+
+    var $loading = selector.query('#loading')[0];
 
     spa
         .if('/page1/', function (ready) {
             require.async('./pages/page1.js', ready);
         })
         .if('/page2/:pageId/', function (ready) {
+            require.async('./pages/page1.js');
+            require.async('./pages/page3.js');
             require.async('./pages/page2.js', ready);
         })
         .if(/^\/page3\/(\d+)\/$/, function (ready) {
@@ -24,23 +30,29 @@ define(function (require, exports, module) {
         .else(function (ready) {
             require.async('./pages/404.js', ready);
         })
-        .before('leave', function (lastRoute, params, query) {
-            console.log(this.alienEmitter.type, arguments);
+        //.before('leave', function (lastRoute, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        //.after('leave', function (lastRoute, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        //.before('enter', function (route, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        //.after('enter', function (route, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        //.before('update', function (route, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        //.after('update', function (route, params, query) {
+        //    console.log(this.alienEmitter.type, arguments);
+        //})
+        .before('loading', function () {
+            attribute.show($loading);
         })
-        .after('leave', function (lastRoute, params, query) {
-            console.log(this.alienEmitter.type, route);
-        })
-        .before('enter', function (route) {
-            console.log(this.alienEmitter.type, route);
-        })
-        .after('enter', function (params, query) {
-            console.log(this.alienEmitter.type, params);
-        })
-        .before('update', function (params, query) {
-            console.log(this.alienEmitter.type, params);
-        })
-        .after('update', function (route) {
-            console.log(this.alienEmitter.type, route);
+        .after('loading', function () {
+            attribute.hide($loading);
         });
 
     document.getElementById('btn1').onclick = function () {
@@ -50,4 +62,6 @@ define(function (require, exports, module) {
     document.getElementById('btn2').onclick = function () {
         spa.redirect('/page2/' + Date.now() + '?c=' + Date.now(), false);
     };
+
+    attribute.hide($loading);
 });
