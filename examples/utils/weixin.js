a4f36c7e97ed4f3ec508b4e5009c56699dc838d4
cloudcome <cloudcome@163.com>
2015-12-15 10:47:48 +0800

    weixn

---
+++ examples/utils/weixin.js
@@ -0,0 +1,18 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-12-15 10:47
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/weixin
+     */
+
+    'use strict';
+
+    var abc = require('./abc.js');
+
+    module.exports = {};
+});
---
+++ src/utils/weixin.js
@@ -0,0 +1,169 @@
+/**
+ * weixin JS SDK
+ * @author ydr.me
+ * @create 2015-12-15 10:40
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module utils/weixin
+     */
+
+    'use strict';
+
+    var wx = require('../3rd/weixin.js');
+    var klass = require('./class.js');
+    var dato = require('./dato.js');
+    var Emitter = require('../libs/emitter.js');
+    var jsApiList = ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'startRecord', 'stopRecord', 'onVoiceRecordEnd', 'playVoice', 'pauseVoice', 'stopVoice', 'onVoicePlayEnd', 'uploadVoice', 'downloadVoice', 'chooseImage', 'previewImage', 'uploadImage', 'downloadImage', 'translateVoice', 'getNetworkType', 'openLocation', 'getLocation', 'hideOptionMenu', 'showOptionMenu', 'hideMenuItems', 'showMenuItems', 'hideAllNonBaseMenuItem', 'showAllNonBaseMenuItem', 'closeWindow', 'scanQRCode', 'chooseWXPay', 'openProductSpecificView', 'addCard', 'chooseCard', 'openCard'];
+    var Weixin = klass.extends(Emitter).create({
+        constructor: function (config, shareData) {
+            var the = this;
+
+            the._shareData = {};
+            shareData.imgUrl = shareData.imgUrl || shareData.img;
+            the.className = 'weixin';
+            the._initEvent();
+        },
+
+
+        /**
+         * @param config {Object}
+         * @param config.debug {Boolean} 是否开启调试模式
+         * @param config.appId {String} appId
+         * @param config.timestamp {Number} 签名的时间戳
+         * @param config.nonceStr {String} 生成签名的随机串
+         * @param config.signature {String} 签名
+         * @param [config.jsApiList] {Array} 需要使用的JS接口列表
+         * @returns {Weixin}
+         */
+        config: function (config) {
+            var the = this;
+
+            config.timestamp = config.timestamp || config.timeStamp;
+            the._config = dato.extend({
+                jsApiList: jsApiList
+            }, config);
+            wx.config(the._config);
+
+            return the;
+        },
+
+        /**
+         * 设置分享数据
+         * @param shareData {Object}
+         * @param shareData.title {String} 分享的标题
+         * @param shareData.desc {String} 分享的描述
+         * @param shareData.link {String} 分享的链接
+         * @param shareData.img {String} 分享的图片
+         * @returns {Weixin}
+         */
+        shareData: function (shareData) {
+            var the = this;
+
+            dato.extend(the._shareData, shareData);
+            return the;
+        },
+
+
+        _initEvent: function () {
+            var the = this;
+
+            // success：接口调用成功时执行的回调函数。
+            // fail：接口调用失败时执行的回调函数。
+            // complete：接口调用完成时执行的回调函数，无论成功或失败都会执行。
+            // cancel：用户点击取消时的回调函数，仅部分有用户取消操作的api才会用到。
+            // trigger: 监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+            var callbacks = {
+                success: function (errMsg) {
+                    the.emit('success', errMsg);
+                },
+                fail: function (errMsg) {
+                    the.emit('error', errMsg);
+                },
+                complete: function (errMsg) {
+                    the.emit('complete', errMsg);
+                },
+                cancel: function (errMsg) {
+                    the.emit('cancel', errMsg);
+                },
+                trigger: function (errMsg) {
+                    the.emit('trigger', errMsg);
+                }
+            };
+            var shareData = dato.extend(the._shareData, callbacks);
+
+            wx.ready(function () {
+                the.emit('ready');
+
+                // 分享到朋友圈
+                wx.onMenuShareTimeline(shareData);
+
+                // 分享给好友
+                wx.onMenuShareAppMessage(shareData);
+
+                // 分享到 QQ
+                wx.onMenuShareQQ(shareData);
+
+                // 分享到腾讯微博
+                wx.onMenuShareWeibo(shareData);
+            });
+
+            wx.error(function (res) {
+                the.emit('error', res);
+            });
+
+            // 旧接口
+            document.addEventListener('WeixinJSBridgeReady', function () {
+                var WeixinJSBridge = window.WeixinJSBridge;
+
+                if (!WeixinJSBridge) {
+                    return;
+                }
+
+                the.emit('ready');
+
+                //绑定‘分享给朋友’按钮
+                WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+                    WeixinJSBridge.invoke('sendAppMessage', {
+                        "appid": the._config.appId,
+                        "img_url": shareData.img,
+                        "link": shareData.link,
+                        "desc": shareData.desc,
+                        "title": shareData.title
+                    }, function (res) {
+                        the.emit('complete', res);
+                    });
+                });
+
+                //绑定‘分享到朋友圈’按钮
+                WeixinJSBridge.on('menu:share:timeline', function (argv) {
+                    WeixinJSBridge.invoke('shareTimeline', {
+                        "img_url": shareData.img,
+                        "link": shareData.link,
+                        "desc": shareData.desc,
+                        "title": shareData.title
+                    }, function (res) {
+                        the.emit('complete', res);
+                    });
+                });
+
+                //绑定‘分享到微博’按钮
+                WeixinJSBridge.on('menu:share:weibo', function (argv) {
+                    WeixinJSBridge.invoke('shareWeibo', {
+                        "content": shareData.desc,
+                        "url": shareData.link
+                    }, function (res) {
+                        the.emit('complete', res);
+                    });
+                });
+            });
+        }
+    });
+
+    var weixin = new Weixin();
+
+    weixin.original = wx;
+    module.exports = weixin;
+});
