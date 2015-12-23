7a7fffb547cac89497da3d73ad263df317f19bed
cloudcome <cloudcome@163.com>
2015-12-23 16:42:07 +0800

    textarea

---
+++ src/utils/textarea.js
@@ -0,0 +1,15 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-12-23 16:40
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/textarea
+     */
+
+    'use strict';
+
+});
