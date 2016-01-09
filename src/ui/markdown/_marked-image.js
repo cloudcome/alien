4d1df940b5fb523de07293413c10bd88d9aa8c7f
cloudcome <cloudcome@163.com>
2016-01-09 10:53:56 +0800

    alien markdown

--- src/ui/markdown/_marked-image.js
+++ src/ui/markdown/_marked-image.js
@@ -9,28 +9,39 @@ define(function (require, exports, module) {
     'use strict';
 
     var typeis = require('../../utils/typeis.js');
+    var dato = require('../../utils/dato.js');
+
     var REG_SIZE = /(?:\s+?=\s*?(\d+)(?:[x*×](\d+))?)?$/i;
+    var defaults = {
+        className: ''
+    };
 
     // ![](1.png =200x100)
-    module.exports = function (src, title, text) {
-        src = src || '';
+    module.exports = function (options) {
+        options = dato.extend({}, defaults, options);
 
-        var matches = src.match(REG_SIZE);
-        var width = null;
-        var height = null;
+        return function (src, title, text) {
+            src = src || '';
 
-        if (matches) {
-            width = matches[1];
-            height = matches[2];
-            src = src.replace(REG_SIZE, '');
-        }
+            var matches = src.match(REG_SIZE);
+            var width = null;
+            var height = null;
 
-        return '<img' +
-            (typeis.null(title) ? '' : ' title="' + title + '"') +
-            (typeis.null(text) ? '' : ' alt="' + text + '"') +
-            (typeis.null(src) ? '' : ' src="' + src + '"' ) +
-            (typeis.null(width) ? '' : ' width="' + width + '"') +
-            (typeis.null(height) ? '' : ' height="' + height + '"' ) +
-            '>';
+            if (matches) {
+                width = matches[1];
+                height = matches[2];
+                src = src.replace(REG_SIZE, '');
+            }
+
+            return ''.concat(
+                '<img class="' + options.className + '"',
+                typeis.empty(title) ? '' : ' title="' + title + '"',
+                typeis.empty(text) ? '' : ' alt="' + text + '"',
+                typeis.empty(src) ? '' : ' src="' + src + '"',
+                typeis.empty(width) ? '' : ' width="' + width + '"',
+                typeis.empty(height) ? '' : ' height="' + height + '"',
+                '>'
+            );
+        };
     };
 });
